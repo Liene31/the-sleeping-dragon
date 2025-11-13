e289f03f1add2918f1d47a4b1d975beffc79598a
Liene <l.bare@interface3.be>
2025-11-13 08:36:28 +0100

    update hamburger menu

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const menuDiv = document.getElementById("menu");
 const hamburgerMenuDiv = document.getElementById("hamburger-menu");
+const closeMenuImg = document.getElementById("close-menu");
 
 // Opens menu on the smaller screens
 
 hamburgerMenuDiv.addEventListener("click", () => {
   menuDiv.style.display = "block";
+  closeMenuImg.style.display = "block";
 });
