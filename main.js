c71c3285ef793126f3caf85134099fd43e3c75a9
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-11-15 15:34:07 +0100

    add hamburger menu toggle functionality

---
+++ main.js
@@ -0,0 +1,27 @@
+const hamburgerMenuBtn = document.getElementById("hamburger-menu");
+
+let isHamburgerMenuClicked = true;
+
+//Toggle hamburger menu
+hamburgerMenuBtn.addEventListener("click", () => {
+  const hamburgerMenuIconSpan = document.getElementById("hamburger-menu-icon");
+  const closeMenuIconSpan = document.getElementById("close-menu-icon");
+
+  if (isHamburgerMenuClicked) {
+    hamburgerMenuIconSpan.style.display = "none";
+    closeMenuIconSpan.style.display = "block";
+    isHamburgerMenuClicked = false;
+
+    if (window.matchMedia("(max-width: 650px)").matches) {
+      document.querySelector(".menu").style.display = "flex";
+    }
+  } else {
+    hamburgerMenuIconSpan.style.display = "block";
+    closeMenuIconSpan.style.display = "none";
+    isHamburgerMenuClicked = true;
+
+    if (window.matchMedia("(max-width: 650px)").matches) {
+      document.querySelector(".menu").style.display = "none";
+    }
+  }
+});
