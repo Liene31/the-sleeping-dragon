2f4ca44a5c044bf632d2edcb12f3f2c7bdd62d9d
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-11-19 17:59:22 +0100

    add hamburger menu

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ const qwertyArray = [
   ["Z", "X", "C", "V", "B", "N", "M"],
 ];
 
+let isHamburgerMenuClicked = true;
+
+//Toggle hamburger menu
+document.getElementById("hamburger-menu").addEventListener("click", () => {
+  const hamburgerMenuIconSpan = document.getElementById("hamburger-menu-icon");
+  const closeMenuIconSpan = document.getElementById("close-menu-icon");
+
+  if (isHamburgerMenuClicked) {
+    hamburgerMenuIconSpan.style.display = "none";
+    closeMenuIconSpan.style.display = "block";
+    isHamburgerMenuClicked = false;
+
+    if (window.matchMedia("(max-width: 650px)").matches) {
+      document.querySelector(".menu").style.display = "flex";
+    }
+  } else {
+    hamburgerMenuIconSpan.style.display = "block";
+    closeMenuIconSpan.style.display = "none";
+    isHamburgerMenuClicked = true;
+
+    if (window.matchMedia("(max-width: 650px)").matches) {
+      document.querySelector(".menu").style.display = "none";
+    }
+  }
+});
+
 function openDifficultyModal() {
   difficultyLevelModalSection.style.display = "flex";
 }
